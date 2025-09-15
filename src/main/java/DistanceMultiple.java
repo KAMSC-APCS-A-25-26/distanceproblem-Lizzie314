59780b6b3fe34b5b27a616a1a31bc1f4626972cd
Lizzie314 <elizabethbippley@gmail.com>
2025-09-15 17:01:39 -0400

    Update DistanceMultiple.java

--- src/main/java/DistanceMultiple.java
+++ src/main/java/DistanceMultiple.java
@@ -47,6 +47,7 @@ public class DistanceMultiple {
 
             // Store the inputs and separate out unwanted parts
             String[] pairs = scanner.nextLine().replaceAll("[()]", "").split(" ");
+            String[] pairs = scanner.nextLine().replaceAll("[()]", " ").split("  ");
 
             // Take out commas
             String[] set1 = pairs[0].split(",");
